a77a8f2acb33aa152cac64a49727028ffcdcf984
unknown <minonna@mail.ru>
2022-05-27 19:41:59 +0300

    feat: dark theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,15 +31,47 @@ function scrollActive() {
     } else {
       document
         .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.add('active-link');
+        .classList.remove('active-link');
     }
   });
 }
 window.addEventListener('scroll', scrollActive);
 /*SHOW SCROLL TOP*/
 
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top');
+  if (this.scrollY >= 200) {
+    scrollTop.classList.add('show-scroll');
+  } else {
+    scrollTop.classList.remove('show-scroll');
+  }
+}
+window.addEventListener('scroll', scrollTop);
 /*DARK LIGHT THEME */
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+const getCurrentTheme = () =>
+  document.body.classList(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'box-moon' : 'bx-sun';
 
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
 /*REDUCE THE SIZE AND PRINT ON AN 4A SHEET*/
 
 /*REMOVE THE SIZE WHEN THE CV IZ DOWNLOADED*/
