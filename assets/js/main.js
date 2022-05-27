543d2205b51ee37751620b4d83dc05673eb43a31
unknown <minonna@mail.ru>
2022-05-27 21:50:17 +0300

    feat: add generate pdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,5 +73,31 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-icon', getCurrentIcon());
 });
 /*REDUCE THE SIZE AND PRINT ON AN 4A SHEET*/
-
+function scaleCV() {
+  document.body.classList.add('scale-cv');
+}
 /*REMOVE THE SIZE WHEN THE CV IZ DOWNLOADED*/
+function removeScale() {
+  document.body.classList.remove('scale-cv');
+}
+/*GENERATE PDF*/
+let areaCv = document.getElementById('area-cv');
+let resumeButton = document.getElementById('resume-button');
+
+let opt = {
+  margin: 0,
+  filename: 'OlerinskaiaCV.pdf',
+  image: { type: 'jpeg', quality: 0.98 },
+  html2canvas: { scale: 4 },
+  jsPDF: { format: 'a4', orientation: 'portrait' },
+};
+
+function generateResume() {
+  html2pdf(areaCv, opt);
+}
+
+resumeButton.addEventListener('click', () => {
+  scaleCV();
+  generateResume();
+  setTimeout(removeScale, 5000);
+});
